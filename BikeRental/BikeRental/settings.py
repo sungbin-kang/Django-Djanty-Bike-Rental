5fbb88c9da608ca28ff09be46d874a3f2d67bdd0
Sungbin_Kang <sungbin9353@gmail.com>
2021-11-15 13:34:35 -0800

    package decouple uninstalled and python-decouple installed

--- BikeRental/BikeRental/settings.py
+++ BikeRental/BikeRental/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+# import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +28,6 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
